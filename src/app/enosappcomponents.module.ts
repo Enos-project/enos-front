a6462e6040e323a13cb1832d845d713882c4ead1
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2018-07-19 20:59:32 +0200

    NG Conf + assets

---
+++ src/app/enosappcomponents.module.ts
@@ -0,0 +1,63 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatGridListModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatSnackBarModule,
+  MatTooltipModule,
+  MatTableModule,
+  MatListModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatMenuModule,
+  MatRadioModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTabsModule,
+  MatStepperModule,
+  MatSlideToggleModule,
+  MatBadgeModule,
+  MatChipsModule,
+} from '@angular/material';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatListModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTabsModule,
+    MatStepperModule,
+    MatSlideToggleModule,
+    MatBadgeModule,
+    MatChipsModule
+  ]
+})
+export class EnosAppComponentsModule { }
